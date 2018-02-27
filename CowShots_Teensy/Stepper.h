c020988449e3966e13e695efd6e607b6c950ea6b
OrianaPeltzer <oriana.peltzer@ensam.eu>
2018-02-27 10:50:00 -0800

    Added stepper motor file

---
+++ CowShots_Teensy/Stepper.h
@@ -0,0 +1,15 @@
+/*---------------Includes-----------------------------------*/
+#include <AccelStepper.h>
+
+/*---------------Module Variables---------------------------*/
+AccelStepper stepmotor(1, 3, 4); // pin 3 = step, pin 4 = direction
+int distance = 10;
+
+void Setup_Stepper(void) {
+  stepmotor.setMaxSpeed(2000);
+}
+
+void runmotor(void) {
+  mystepper.move(distance);
+}
+
